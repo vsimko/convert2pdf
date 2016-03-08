8aedc4983e98896060ef0e67c3d66c21599761ba
Viliam Simko <viliam.simko@gmail.com>
2016-03-09 00:32:10 +0100

    SVG input format

---
+++ .figconv-plugins/svg-input.sh
@@ -0,0 +1,17 @@
+# Support for Scalable Vector Graphics (SVG) input format
+
+add_input_ext svg
+
+function convert_svg_to_pdf() {
+  # unfortunately, SVG filters such as Duotone will be resterized
+  inkscape --export-dpi=500 --export-pdf="$2" "$1" 
+#2> /dev/null
+}
+
+function check_svg() {
+  cmdavail inkscape || {
+    echo "Inkscape is not installed. It is required for SVG->PDF conversion."
+    echo "Try to install inkscape, e.g. sudo apt-get install inkscape."
+		exit $CHECK_FAILED
+  }
+}
