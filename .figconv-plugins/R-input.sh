3e645e385137a34538909d1cf2f7247ad3e92dc9
Viliam Simko <viliam.simko@gmail.com>
2016-03-09 00:32:01 +0100

    R input format

---
+++ .figconv-plugins/R-input.sh
@@ -0,0 +1,22 @@
+# Support for plot generated in R language
+
+add_input_ext R
+
+function convert_R_to_pdf() {
+	Rscript --vanilla - <<===
+		source('$1')
+		cat('R script loaded\n')
+		cat(paste('Creating PDF of size', pdf.width, 'x', pdf.height, '\n'))
+		pdf('$2', width=pdf.width, height=pdf.height)
+		cat('Now running R code that should plot graphics to the PDF\n')
+		pdf.plot()
+		cat('R code done.\n')
+===
+}
+
+function check_R() {
+  cmdavail Rscript || {
+    echo "R not installed"
+		exit $CHECK_FAILED
+  }
+}
