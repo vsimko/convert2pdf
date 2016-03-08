f30075739a23878d83616cb3ac9f5b9613206224
Viliam Simko <viliam.simko@gmail.com>
2016-03-09 00:32:33 +0100

    PNG output format

---
+++ .figconv-plugins/png-output.sh
@@ -0,0 +1,20 @@
+# Support for PNG output format
+
+add_output_ext png
+
+function convert_pdf_to_png() {
+  PDFFILE="$1"
+  PNGFILE="$2"
+  echo "Converting PDF=$PDFFILE to PNG=$PNGFILE ..."
+#  convert -density 150 "$PDFFILE" -quality 90 "$PNGFILE"
+#  inkscape "$PDFFILE" -z --export-dpi=300 --export-area-drawing --export-png="$PNGFILE"
+  gs -sDEVICE=png16m -dTextAlphaBits=4 -r300 -o "$PNGFILE" "$PDFFILE"
+}
+
+function check_png() {
+  cmdavail gs || {
+    echo "Ghostscript not installed (gs command)"
+		exit $CHECK_FAILED
+  }
+}
+
