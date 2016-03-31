0666cf299dde0ab212828e3b3724964bc9188fee
Viliam Simko <viliam.simko@gmail.com>
2016-03-31 19:14:00 +0200

    fixing issue #20

--- share/figconv-plugins/system-checks.sh
+++ share/figconv-plugins/system-checks.sh
@@ -1,11 +1,20 @@
 # running checks from all plugins
 function system_check() {
+
+  # first check if we are in the right directory
+  FINDCMD="find . -maxdepth 1 -iname '' "$(get_input_extensions | sed 's/^/-or -iname *./')
+  if [ $(`echo $FINDCMD` | wc -l) -lt 1 ]; then
+    echoerr "This directory does not contain input images"
+    exit 1
+  fi
+
+  # now check all plugins
   for EXT in `get_all_extensions`; do
     echo -n "checking $EXT ... "
     check_$EXT
     ERRCODE=$?
     if [ $ERRCODE -eq $CODE_ERROR ]; then
-      echo "ERROR: failed with return code $ERRCODE"
+      echoerr "failed with return code $ERRCODE"
       exit $ERRCODE
     elif [ $ERRCODE -eq $CODE_WARNING ]; then
       echo # pass
